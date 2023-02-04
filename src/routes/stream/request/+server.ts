4ee495a07ea2cc75ac1be5145535bd87761ca4b6
IqbalLx <iqbal19600@gmail.com>
2023-02-04 19:46:51 +0700

    fix: SSE WORKINGGG

--- src/routes/stream/request/+server.ts
+++ src/routes/stream/request/+server.ts
@@ -2,6 +2,5 @@ import type { RequestHandler } from '@sveltejs/kit';
 import { sentEvent } from '../stream';
 
 export const GET: RequestHandler = () => {
-	const ac = new AbortController();
-	return sentEvent('request', ac);
+	return sentEvent('request');
 };
--- src/routes/stream/stream.ts
+++ src/routes/stream/stream.ts
@@ -2,22 +2,37 @@ import type { HTTPRequest } from 'entities/httpRequest.entity';
 import type { HTTPResponse } from 'entities/httpResponse.entity';
 import { httpEvent } from '../../services/httpEventEmitter';
 
-export function sentEvent(type: 'request' | 'response', ac: AbortController): Response {
+function createCallback<T extends HTTPRequest | HTTPResponse>(
+	ctr: ReadableStreamDefaultController
+) {
+	return (val: T) => ctr.enqueue(`data: ${JSON.stringify(val)}\n\n`);
+}
+
+export function sentEvent(type: 'request' | 'response'): Response {
+	const ac = new AbortController();
+
+	let callback: (val: HTTPRequest | HTTPResponse) => void;
+	httpEvent.on(type, (val: HTTPRequest | HTTPResponse) => callback(val));
+
 	const readable = new ReadableStream({
 		start(ctr) {
-			httpEvent.on(type, (val: HTTPRequest | HTTPResponse) =>
-				ctr.enqueue(`data: ${JSON.stringify(val)}\n\n`)
-			);
+			console.debug(`${type} stream start`);
+
+			callback = createCallback(ctr);
 		},
 		cancel() {
-			console.debug('request cancel');
+			console.debug(`${type} stream cancel`);
+
+			httpEvent.off(type, callback);
 			ac.abort();
 		}
 	});
 
 	return new Response(readable, {
 		headers: {
-			'content-type': 'text/event-stream'
+			'content-type': 'text/event-stream',
+			'Cache-control': 'no-cache',
+			Connection: 'keep-alive'
 		}
 	});
 }
